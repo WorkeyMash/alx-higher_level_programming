1eb350948636168d5adf2347267404e4290b5550
WorkeyMash <mashabelaworkers@gmail.com>
2024-05-03 03:09:02 +0000

    0x15. JavaScript - Web jQuery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Update Header Color</title>
+    <!-- Link to the JavaScript script -->
+    <script src="update_header.js"></script>
+</head>
+<body>
+    <header>This is the header</header>
+    <p>This is some content in the body of the page.</p>
+</body>
+</html>
+
